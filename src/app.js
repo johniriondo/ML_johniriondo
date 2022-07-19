8f0ada2d6911808bc402d2b095a4466f20d22d16
johniriondo <johniriondo@gmail.com>
2022-07-19 20:16:45 -0300

    primer commit

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const server = express();
+const port = 2020;
+const start = () =>console.log('Starting server...');
+server.listen(port,start());
+
+const {join} = require('path');  //construye la ruta en forma completa
+const public = join (__dirname , '../public');
+const statics= express.static (public);
+server.use(statics);
+
+server.get('/', function (req, res) {
+    let file = join(__dirname, 'views','home.html') 
+    return res.sendFile (file)
+})
+
+server.get('/register', function (req, res) {
+    let file = join(__dirname, 'views','register.html') 
+    return res.sendFile (file)
+})
+
+server.get('/login', function (req, res) {
+    let file = join(__dirname, 'views','login.html') 
+    return res.sendFile (file)
+})
